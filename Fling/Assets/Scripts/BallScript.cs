44d283810787bfc9b5535431617f04b7a07db521
Brian D <dieffenderferbmd@gmail.com>
2017-08-27 22:59:43 -0400

    Began implementing ball moving with paddle

--- Fling/Assets/Scripts/BallScript.cs
+++ Fling/Assets/Scripts/BallScript.cs
@@ -6,16 +6,27 @@ public class BallScript : MonoBehaviour {
 
     GameManager gameManager;
 
-    GameObject connectedTo;
+    public GameObject connectedTo;
+
+    Vector3 offset;
+    bool firstShot = true;
 
 	// Use this for initialization
 	void Start () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        offset = new Vector3(connectedTo.transform.position.x + transform.position.x, connectedTo.transform.position.y + .2f, connectedTo.transform.position.z + transform.position.z);
+
+
+        if (gameManager.canShoot && !firstShot)
+        {
+            //transform.position = offset;
+            transform.position = new Vector3(connectedTo.transform.position.x + transform.position.x, connectedTo.transform.position.y + .2f, connectedTo.transform.position.z + transform.position.z);
+        }
 		
 	}
 
@@ -31,6 +42,11 @@ public class BallScript : MonoBehaviour {
         {
             gameManager.GameOver();
         }
+        if (col.gameObject.name == "BallDestroy")
+        {
+            gameManager.GameOver();
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -38,14 +54,16 @@ public class BallScript : MonoBehaviour {
         if(other.gameObject.tag == "Paddle")
         {
             connectedTo = other.gameObject;
-            //gameObject.transform.parent = connectedTo.transform;
-
             gameManager.canShoot = false;
             gameManager.score++;
             gameManager.scoreText.text = gameManager.score.ToString();
             gameManager.ballSpeed = 0;
             gameManager.scrolling = true;
             transform.position = new Vector3(transform.position.x, connectedTo.transform.position.y + .2f, transform.position.z);
+            offset = new Vector3(connectedTo.transform.position.x + transform.position.x, connectedTo.transform.position.y + .2f, connectedTo.transform.position.z + transform.position.z);
+            firstShot = false;
+
+            //gameObject.transform.parent = connectedTo.transform;
         }
     }
 }
