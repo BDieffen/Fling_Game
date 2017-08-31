4a819cc8aec815d9d424c496de626eac476bb984
Brian D <dieffenderferbmd@gmail.com>
2017-08-30 21:34:32 -0400

    Started adding colors and imported effects

---
+++ Fling/Assets/Scripts/Options/PlayerMatOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerMatOptions : MonoBehaviour {
+
+    public Material[] ballMats = new Material[8];
+    public Material[] paddleMats = new Material[8];
+    public Image[] backgroundImages = new Image[5];
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
