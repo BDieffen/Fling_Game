c896e1008e32312ac1376acc05f9f7fbe9ea93aa
Brian D <dieffenderferbmd@gmail.com>
2017-09-08 09:32:35 -0400

    ogic for options scrolling

--- Fling/Assets/Scripts/Options/PlayerMatOptions.cs
+++ Fling/Assets/Scripts/Options/PlayerMatOptions.cs
@@ -7,17 +7,81 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class PlayerMatOptions : MonoBehaviour {
+public class PlayerMatOptions : MonoBehaviour
+{
 
     public Material[] ballMats = new Material[8];
+    public int ballSelectSpot;
+    public Material selectedBall;
     public Material[] paddleMats = new Material[8];
+    public int paddleSelectSpot;
+    public Material selectedPaddle;
     public Image[] backgroundImages = new Image[5];
 
-    void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void scrollRight(Material[] array)
+    {
+        if (array == ballMats)
+        {
+            if (ballSelectSpot == 7)
+            {
+                ballSelectSpot = 0;
+            }
+            else
+            {
+                ballSelectSpot++;
+            }
+            selectedBall = ballMats[ballSelectSpot];
+        }
+        else if (array == paddleMats)
+        {
+            if (paddleSelectSpot == 7)
+            {
+                paddleSelectSpot = 0;
+            }
+            else
+            {
+                paddleSelectSpot++;
+            }
+            selectedPaddle = paddleMats[paddleSelectSpot];
+        }
+    }
+
+    public void scrollLeft(Material[] array)
+    {
+        if (array == ballMats)
+        {
+            if (ballSelectSpot == 0)
+            {
+                ballSelectSpot = 7;
+            }
+            else
+            {
+                ballSelectSpot--;
+            }
+            selectedBall = ballMats[ballSelectSpot];
+        }
+        else if (array == paddleMats)
+        {
+            if (paddleSelectSpot == 0)
+            {
+                paddleSelectSpot = 7;
+            }
+            else
+            {
+                paddleSelectSpot--;
+            }
+            selectedPaddle = paddleMats[paddleSelectSpot];
+        }
+    }
 }
